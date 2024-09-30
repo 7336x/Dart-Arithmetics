f8c265f9aa6384e741dbd2c22a46da23258f6da0
7336x <R7664alhussaini@gmail.com>
2024-09-30 10:08:43 +0300

    initial commit

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main ()
+{
+  double tempInFahrenheit = 86 ;
+ double  Celsius = ( tempInFahrenheit - 32) / 1.8 ;
+
+ print(" $tempInFahrenheit F = $Celsius C");
+
+  print("$Celsius C = $tempInFahrenheit F");
+}
